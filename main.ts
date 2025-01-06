db8e01de88703e35de20a7e5175873b4c0792d23
EliaDusin <elia.dusin.2010@gmail.com>
2025-01-06 09:15:44 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Alarmton = 1
-    music.play(music.stringPlayable("B F A - B F A - ", 120), music.PlaybackMode.UntilDone)
+    basic.setLedColor(0xff8000)
+    music.play(music.stringPlayable("C C D D E E C C ", 120), music.PlaybackMode.UntilDone)
     basic.pause(1000)
     Alarm = 1
-    motors.dualMotorPower(Motor.M0, 100)
+    basic.setLedColor(0x00ff00)
     basic.pause(5000)
-    motors.dualMotorPower(Motor.M0, -100)
-    Alarm = 0
+    basic.setLedColor(0xff0000)
     Alarmton = 0
 })
 music.onEvent(MusicEvent.MelodyStarted, function () {
@@ -43,15 +43,20 @@ music.onEvent(MusicEvent.MelodyStarted, function () {
         }
     }
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    Alarm = 1
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Alarm = 0
+})
 let Alarm = 0
 let Alarmton = 0
+basic.setLedColor(0xff0000)
 Alarmton = 0
 Alarm = 1
-basic.pause(2000)
-Alarm = 0
 basic.forever(function () {
-    if (input.lightLevel() > 80) {
-        if (Alarm == 0) {
+    if (Alarm == 0) {
+        if (input.lightLevel() > 120) {
             music.play(music.stringPlayable("C5 E C5 - C5 E C5 - ", 150), music.PlaybackMode.UntilDone)
             music.play(music.stringPlayable("C5 E C5 - C5 E C5 - ", 150), music.PlaybackMode.UntilDone)
         }
