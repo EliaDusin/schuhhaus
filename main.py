9607e1b01f8d5b32256da3b22cbeb2a7831c3c6c
EliaDusin <elia.dusin.2010@gmail.com>
2025-01-20 09:07:37 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,57 +1,11 @@
 def on_button_a():
-    global Alarm
-    music.play(music.string_playable("B F A - B F A - ", 120),
+    music.play(music.string_playable("C C D D E E C C ", 120),
         music.PlaybackMode.UNTIL_DONE)
-    basic.pause(1000)
-    Alarm = 1
-    motors.dual_motor_power(Motor.M0, 100)
-    basic.pause(5000)
-    motors.dual_motor_power(Motor.M0, -100)
-    Alarm = 0
 input.on_button_event(Button.A, input.button_event_click(), on_button_a)
 
-def on_melody_started():
-    for index in range(2):
-        basic.show_leds("""
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            """)
-        basic.show_leds("""
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            """)
-        basic.show_leds("""
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            """)
-        basic.show_leds("""
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-music.on_event(MusicEvent.MELODY_STARTED, on_melody_started)
-
-Alarm = 0
-Alarm = 1
-basic.pause(2000)
-Alarm = 0
-
 def on_forever():
-    if input.light_level() > 80:
-        if Alarm == 0:
-            music.play(music.string_playable("C5 E C5 - C5 E C5 - ", 150),
-                music.PlaybackMode.UNTIL_DONE)
-            music.play(music.string_playable("C5 E C5 - C5 E C5 - ", 150),
-                music.PlaybackMode.UNTIL_DONE)
+    if input.light_level() < 20:
+        basic.set_led_colors(0xffffff, 0xffffff, 0xffffff)
+    if input.light_level() > 20:
+        basic.set_led_colors(0x000000, 0x000000, 0x000000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,64 +1,11 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Alarmton = 1
-    basic.setLedColor(0xff8000)
     music.play(music.stringPlayable("C C D D E E C C ", 120), music.PlaybackMode.UntilDone)
-    basic.pause(1000)
-    Alarm = 1
-    basic.setLedColor(0x00ff00)
-    basic.pause(5000)
-    basic.setLedColor(0xff0000)
-    Alarmton = 0
 })
-music.onEvent(MusicEvent.MelodyStarted, function () {
-    if (Alarmton == 0) {
-        for (let index = 0; index < 2; index++) {
-            basic.showLeds(`
-                # . # . #
-                . # . # .
-                # . # . #
-                . # . # .
-                # . # . #
-                `)
-            basic.showLeds(`
-                . # . # .
-                # . # . #
-                . # . # .
-                # . # . #
-                . # . # .
-                `)
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-        }
-    }
-})
-input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    Alarm = 1
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    Alarm = 0
-})
-let Alarm = 0
-let Alarmton = 0
-basic.setLedColor(0xff0000)
-Alarmton = 0
-Alarm = 1
 basic.forever(function () {
-    if (Alarm == 0) {
-        if (input.lightLevel() > 120) {
-            music.play(music.stringPlayable("C5 E C5 - C5 E C5 - ", 150), music.PlaybackMode.UntilDone)
-            music.play(music.stringPlayable("C5 E C5 - C5 E C5 - ", 150), music.PlaybackMode.UntilDone)
-        }
+    if (input.lightLevel() < 20) {
+        basic.setLedColors(0xffffff, 0xffffff, 0xffffff)
+    }
+    if (input.lightLevel() > 20) {
+        basic.setLedColors(0x000000, 0x000000, 0x000000)
     }
 })
